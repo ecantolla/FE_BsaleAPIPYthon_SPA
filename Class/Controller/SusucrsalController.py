1e27384766f5e4c840bf64471bde760976b08573
lilianac <mariali_co@hotmail.com>
2024-07-27 21:33:27 -0500

    Ajuste controlador usuario y sucursal

--- Class/Controller/SusucrsalController.py
+++ Class/Controller/SusucrsalController.py
@@ -1,5 +1,6 @@
 from Class.Models.tablas import tablas
-from Class.ConnectionHandler import ConnectionHandler
+from Class.Controller.AbstractController import AbstractController
+from Class.Controller.Herlpers import *
 import requests
 import json
 import os
@@ -7,79 +8,54 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class SucursalController:
-    def __init__(self):
-        self.table=tablas["sucursal"]
-        self.datas=[]
+class SucursalController(AbstractController):
+    def __init__(self, tabla):
+        super().__init__(tabla)
+
     def cleanData(self):
         query=f"""delete from {self.table}"""
         return query
+
     def getData(self):
         url = os.getenv('API_URL_BASE') + '/offices.json'
-        flag=True
         headers = {'Accept': 'application/json','access_token':os.getenv('API_KEY')}
-        while(flag):
-            req = requests.get(url, headers=headers)
-            response=json.loads(req.text)
-            if("next" in response):
-                flag=True
-                url=response["next"]+''
-            else:
-                flag=False
-            for current in response["items"]:
-                self.datas.append(current)
-    def getInsertQuery(self):
-        query=f"""INSERT INTO {self.table}
-                ([id]
-                ,[name]
-                ,[description]
-                ,[address]
-                ,[latitude]
-                ,[longitude]
-                ,[isVirtual]
-                ,[country]
-                ,[municipality]
-                ,[city]
-                ,[zipCode]
-                ,[email]
-                ,[costCenter]
-                ,[state]
-                ,[imagestionCellarId]
-                ,[defaultPriceList])
-            VALUES"""
-        for current in self.datas:
-            query=query+f"""
-                ({current["id"]}
-                ,'{current["name"]}'
-                ,'{current["description"]}'
-                ,'{current["address"]}'
-                ,'{current["latitude"]}'
-                ,'{current["longitude"]}'
-                ,{current["isVirtual"]}
-                ,'{current["country"]}'
-                ,'{current["municipality"]}'
-                ,'{current["city"]}'
-                ,'{current["zipCode"]}'
-                ,'{current["email"]}'
-                ,'{current["costCenter"]}'
-                ,{current["state"]}
-                ,{current["imagestionCellarId"]}
-                ,{current["defaultPriceList"]}),"""
 
-        query=query.replace("'None'",'null')
-        query=query[:-1]
-        return query
-    def executeQuery(self,query):
-        conn=ConnectionHandler()
-        conn.connect()
-        conn.executeQuery(query)
-        conn.commitChange()
-        conn.closeConnection()
+        while True:
+            req = requests.get(url, headers=headers)
+            response = json.loads(req.text)
+            for current in response["items"]:                
+                current = format_record(current, self.cols, self.ctypes)
+                if self.row_exists(current):
+                    print(f"la sucursal {current['id']} ya existe")
+                    continue
+                self.datas.append(current)
+            if "next" in response:
+                url = response["next"]
+            else:
+                break
+
+    def getInsertQuery(self):
+        query = f'INSERT INTO {self.table} '
+        query += '(' + ','.join([f'[{c}]' for c in self.cols]) + ')'
+        query += f' VALUES (' + ','.join(['?' for c in range(len(self.cols))]) + ')'
+        
+        values = []
+        for i, current in enumerate(self.datas, 1):
+            vals = tuple([current[c] for c in self.cols])
+            values.append(vals)
+        try:
+            self.execute_query(query, 'insert', values)
+        except Exception as e:
+            print('no se insertaron datos.')
+            print(e)
+
+        values = []
+
     def executelogic(self):
-        print("Limpiando sucursales")
-        self.executeQuery(self.cleanData())
+        # print("Limpiando sucursales")
+        # self.executeQuery(self.cleanData())
         print("Obteniendo sucursales")
         self.getData()
         print("Generando Query")
         query=self.getInsertQuery()
-        self.executeQuery(query)
+        #self.executeQuery(query)
--- Class/Controller/UsuarioController.py
+++ Class/Controller/UsuarioController.py
@@ -1,5 +1,6 @@
 from Class.Models.tablas import tablas
 from Class.ConnectionHandler import ConnectionHandler
+from Class.Controller.Herlpers import *
 import requests
 import json
 import os
@@ -10,8 +11,8 @@ load_dotenv()
 
 
 class UsuarioController(AbstractController):
-    def __init__(self):
-        super().__init__('usuario')
+    def __init__(self, tabla):
+        super().__init__(tabla)
         self.offset = 0
     def cleanData(self):
         query=f"""delete from {self.table}"""
@@ -20,50 +21,47 @@ class UsuarioController(AbstractController):
         url = os.getenv('API_URL_BASE') + '/users.json?limit=50&offset='+str(self.offset)
         flag=True
         headers = {'Accept': 'application/json','access_token':os.getenv('API_KEY')}
-        while(flag):
+        while True:
             req = requests.get(url, headers=headers)
-            response=json.loads(req.text)
-            if("next" in response):
-                flag=True
-                url=response["next"]+''
-            else:
-                flag=False
+            response = json.loads(req.text)
             for current in response["items"]:
-                
+                if 'office' in current:
+                    current['idSucursal'] = current['office']['id']
+                    del current['office']
+                # print(current)
+                # breakpoint()
+                current = format_record(current, self.cols, self.ctypes)
+
+                if self.row_exists(current):
+                    print(f"El usuario {current['id']} ya existe")
+                    continue
+
                 self.datas.append(current)
+            if "next" in response:
+                url = response["next"]
+            else:
+                break
+
     def getInsertQuery(self):
-        query=f"""
-            INSERT INTO {self.table}
-                ([id]
-                ,[firstName]
-                ,[lastName]
-                ,[email]
-                ,[state]
-                ,[idSucursal])
-            VALUES
-        """
-        for current in self.datas:
-            query=query+ f"""
-                ({current["id"]}
-                ,'{current["firstName"]}'
-                ,'{current["lastName"]}'
-                ,'{current["email"]}'
-                ,{current["state"]}
-                ,{current["office"]["id"]}),"""
-        query=query.replace("'None'",'null')
-        query=query[:-1]
-        return query
-    def executeQuery(self,query):
-        conn=ConnectionHandler()
-        conn.connect()
-        conn.executeQuery(query)
-        conn.commitChange()
-        conn.closeConnection()
+        query = f'INSERT INTO {self.table} '
+        query += '(' + ','.join([f'[{c}]' for c in self.cols]) + ')'
+        query += f' VALUES (' + ','.join(['?' for c in range(len(self.cols))]) + ')'
+        values = []
+        for i, current in enumerate(self.datas, 1):
+            vals = tuple([current[c] for c in self.cols])
+            values.append(vals)
+        try:
+            self.execute_query(query, 'insert', values)
+        except Exception as e:
+            print('no se insertaron datos de usuario.')
+            print(e)
+
+        values = []
+
     def executelogic(self):
-        print("Limpiando Usuario")
-        self.executeQuery(self.cleanData())
+        # print("Limpiando Usuario")
+        # self.executeQuery(self.cleanData())
         print("Obteniendo usuarios")
         self.getData()
         print("Generando Query")
         query=self.getInsertQuery()
-        self.executeQuery(query)
