30c569642474c88ef5b9ba991b20d800fae0221e
Jesus Rodriguez <jarh1992@gmail.com>
2024-07-30 22:26:50 -0500

    uploade fix

--- generalTest.py
+++ generalTest.py
@@ -17,26 +17,32 @@ from datetime import datetime
 
 
 inicio = datetime.now()
+
 print("##########################################")
 print("Inicio sucursal")
 sucursal = SucursalController('sucursal')
 sucursal.execute_logic()
+
 print("##########################################")
 print("Inicio usuario")
 usuario = UsuarioController('usuario')
 usuario.execute_logic()
+
 print("##########################################")
 print("Inicio Descuento")
 descuento = DescuentoController('descuento')
 descuento.execute_logic()
+
 print("##########################################")
 print("Inicio Tipo Libro")
 tlc = TipoLibroController('tipoLibro')
 tlc.execute_logic()
+
 print("##########################################")
 print("Inicio Tipo Documento")
 tdc = TipoDocumentoController('tipoDocumento')
 tdc.execute_logic()
+
 print("##########################################")
 print("Inicio Tipo Producto")
 ptc = ProductTypeController('tipoProducto')
@@ -46,10 +52,12 @@ print("##########################################")
 print("Inicio Producto")
 pc = ProductController('producto')
 pc.execute_logic()
+
 print("##########################################")
 print("Inicio Variante")
 vc = VarianteController('variante')
 vc.execute_logic()
+
 print("##########################################")
 print("Inicio Lista precio")
 lpc = ListaPrecioController('listaPrecio')
@@ -65,10 +73,10 @@ print("Inicio Recepcion")
 rc = RecepcionController('recepcion')
 rc.execute_logic()
 
-# print("##########################################")
-# print("Inicio Consumo")
-# cc = ConsumoController()
-# cc.executelogic()
+print("##########################################")
+print("Inicio Consumo")
+cc = ConsumoController('consumo')
+cc.executelogic()
 
 print("##########################################")
 print("Inicio Cliente")
@@ -77,7 +85,7 @@ cc.execute_logic()
 
 print("##########################################")
 print("Inicio Devolucion")
-tp=DevolucionController('devolucion')
-tp.execute_logic()
+dc = DevolucionController('devolucion')
+dc.execute_logic()
 
 print(datetime.now()-inicio)
